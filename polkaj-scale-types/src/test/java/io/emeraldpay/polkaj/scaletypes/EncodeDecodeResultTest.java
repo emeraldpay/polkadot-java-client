abaef5b3fde366013ad722f60de80440436ca15c
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-02-27 14:42:36 +0200

    Add Junit test for result type

--- polkaj-scale-types/src/test/java/io/emeraldpay/polkaj/scaletypes/EncodeDecodeResultTest.java
+++ polkaj-scale-types/src/test/java/io/emeraldpay/polkaj/scaletypes/EncodeDecodeResultTest.java
@@ -2,17 +2,17 @@ package io.emeraldpay.polkaj.scaletypes;
 
 import io.emeraldpay.polkaj.scale.ScaleCodecReader;
 import io.emeraldpay.polkaj.scale.ScaleCodecWriter;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
 
-public class EncodeDecodeResult {
+public class EncodeDecodeResultTest {
 
     @Test
-    public void EncodeDecodeResult() {
+    public void EncodeDecodeResultTest() {
         Result<Integer, Integer> dataToEncode = new Result<>(Result.ResultMode.OK, 10, 5);
         ByteArrayOutputStream buf = new ByteArrayOutputStream();
         try (ScaleCodecWriter writer = new ScaleCodecWriter(buf)) {
@@ -24,9 +24,9 @@ public class EncodeDecodeResult {
             ResultReader<Integer, Integer> resultReader = new ResultReader<>();
             Result<Integer, Integer> result = resultReader.readResult(reader, ScaleCodecReader::readCompactInt, ScaleCodecReader::readCompactInt);
 
-            assertEquals(dataToEncode.mode, result.mode);
-            assertEquals(dataToEncode.getOkValue(), result.getOkValue());
-            assertEquals(null, result.getErrorValue());
+            Assertions.assertEquals(dataToEncode.mode, result.mode);
+            Assertions.assertEquals(dataToEncode.getOkValue(), result.getOkValue());
+            Assertions.assertEquals(null, result.getErrorValue());
         } catch (IOException e) {
             e.printStackTrace();
         }
